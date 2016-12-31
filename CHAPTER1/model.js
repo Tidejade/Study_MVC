12c0fa5c0b151d2fa4d3408cc11e8e1c0532ee29
tide <tidejade@gmail.com>
2016-12-31 15:58:37 +0900

    add the chapter 1

---
+++ CHAPTER1/model.js
@@ -0,0 +1,14 @@
+/*
+// The bellow code is not namespaced or object oriented.
+var user=users["foo"];
+destoryUser(user);
+*/
+
+var user=users.find("foo");
+user.destory();
+
+/*The model is where all the application's data objects are stored.
+  The only thing a model should contain is data and the logic associated directly with that data.
+  When controllers fetch data from servers or create new records, they wrap them in model instances.
+*/
+
