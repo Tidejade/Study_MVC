5b9a62c6f2186d6f4249a97e610136fb405f276f
tide <tidejade@gmail.com>
2017-01-03 11:39:15 +0900

    add the controllers

---
+++ CHAPTER1/controller.js
@@ -0,0 +1,13 @@
+var Controller={};
+
+(Controller.users=function($){
+	var nameClick=function(){
+
+	};
+$(function(){
+	$("#view.name").click(nameClick);
+});
+})(jQuery);
+
+//The Controller receive events and input from views,process them , and update the views accordingly.
+//Controllers are the glue between models and views.
