a460a8ffadfd3d6e3778fd4b371ea3839d010108
Tidejade <tidejade@gmail.com>
2017-02-22 10:44:53 +0900

    Add files via upload

---
+++ export/export.js
@@ -0,0 +1,16 @@
+/*module.export.a =function(){
+	console.log('a called');
+};
+modele.export.b =function(){
+
+	console.log('b called');
+}
+Alias for this is showned below
+*/
+export.a =function(){
+	console.log('a called');
+};
+export.b =function(){
+
+	console.log('b called');
+}
